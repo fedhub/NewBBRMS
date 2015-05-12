11833b8de683357992f5bdc5ea9a6d5fb88348c3
shimonsw <shimi0007@hotmail.com>
2015-05-12 23:23:00 +0300

    WIP - MS Managers

--- includes/js/managers.js
+++ includes/js/managers.js
@@ -2,6 +2,10 @@ var base_url = 'http://localhost:3000';
 
 $(document).ready(function() {
 
+    var $msg_box = $('<div>', {
+        class: 'msg'
+    }).append($('<p>'));
+
     $('#managers').click(function(){
         check_if_admin_ajax();
     });
@@ -10,6 +14,41 @@ $(document).ready(function() {
         $('#error-page').fadeOut(function(){
             location.reload();
         });
+    });
+
+    var selected_array = [];
+    $('select').each(function(){
+        var admin_state = $(this).attr('id');
+        if(admin_state == '0'){
+            $(this).find('option').eq(0).prop('selected', true);
+            selected_array.push(0);
+        }
+        if(admin_state == '1'){
+            $(this).find('option').eq(1).prop('selected', true);
+            selected_array.push(1);
+        }
+    });
+
+    $('.save').click(function(e){
+        var index = $(this).parent().index()-1; // the row index
+        var id = $(e.target).attr('id');
+        var manager_id = id.split('-')[1]; // the manager id in the database
+        var new_admin_state = $('.manager-item').eq(index).find('select').val(); // the updated selection of admin state
+        if(new_admin_state == selected_array[index]){
+            $msg_box.find('p').html('לא בוצע שינוי');
+            $(this).append($msg_box);
+            $msg_box.fadeIn().delay(500).fadeOut(function(){
+                $msg_box.remove();
+            });
+        }
+        else window.location = '/update-admin&manager_id='+manager_id+'&admin_state='+new_admin_state;
+    });
+
+    $('.delete').click(function(e){
+        var index = $(this).parent().index()-1; // the row index
+        var id = $(e.target).attr('id');
+        var manager_id = id.split('-')[1]; // the manager id in the database
+        get_manager_id_ajax($msg_box, index, manager_id);
     });
 
 });
@@ -29,4 +68,22 @@ function not_admin_handler(){
     console.log('here');
     $('#error-msg').html('אין לך הרשאות משתמש מתאימות עבור ניהול המנהלים, הגישה נחסמה');
     $('#error-page').fadeIn();
+}
+
+function get_manager_id_ajax($msg_box, index, manager_id){
+    var url = base_url + '/manager-id&test_id=' + manager_id;
+    $.ajax({
+        url: url,
+        type: 'POST'
+    }).done(function(res){
+        if(res) {  // can't delete himself
+            console.log('here');
+            $msg_box.find('p').html('אינך רשאי למחוק את עצמך');
+            elm.eq(index).append($msg_box);
+            $msg_box.fadeIn().delay(500).fadeOut(function(){
+                $msg_box.remove();
+            });
+        }
+        else window.location = '/delete-manager&manager_id='+manager_id;
+    });
 }
--- managers.js
+++ managers.js
@@ -19,7 +19,7 @@ managers.get('/managers', function(req, res){
                     });
                 }
                 else{
-                    res.status(404).render('error404')
+                    res.status(404).render('error404');
                 }
             });
             conn.release();
@@ -32,6 +32,82 @@ managers.get('/managers', function(req, res){
 managers.post('/is-admin-check', function(req, res){
 
     if(settings.get_is_admin()) res.send(true);
+    else res.send(false);
+
+});
+
+managers.get('/update-admin&:manager_id&:admin_state', function(req, res){
+
+    var breadcrumbs = [{path: '/', name: 'דף הבית'}, {path: '/managers', name: 'מנהלים'}];
+    var id = req.params.manager_id.split('=')[1];
+    var state = req.params.admin_state.split('=')[1];
+
+    var query = 'UPDATE `managers` SET `admin`="'+state+'" WHERE `id`="'+id+'";';
+    mysql.getConnection(function(err, conn){
+        if(!err){
+            conn.query(query, function(err, result){
+                if(!err){
+                    query = 'SELECT * FROM `managers`;';
+                    conn.query(query, function(err, result){
+                        if(!err){
+                            res.render('managers', {
+                                username: settings.get_username(),
+                                breadcrumbs: breadcrumbs,
+                                managers: result
+                            });
+                        }
+                        else{
+                            res.status(404).render('error404');
+                        }
+                    });
+                }
+                else{
+                    res.status(404).render('error404')
+                }
+            });
+            conn.release();
+        }
+        else {console.log(err);}
+    });
+
+});
+
+managers.get('/delete-manager&:manager_id', function(req, res){
+
+    var breadcrumbs = [{path: '/', name: 'דף הבית'}, {path: '/managers', name: 'מנהלים'}];
+    var id = req.params.manager_id.split('=')[1];
+    var query = 'DELETE FROM `managers` WHERE `id`="'+id+'";';
+    mysql.getConnection(function(err, conn){
+        conn.query(query, function(err, result){
+            if(!err){
+                query = 'SELECT * FROM `managers`;';
+                conn.query(query, function(err, result){
+                    if(!err){
+                        res.render('managers', {
+                            username: settings.get_username(),
+                            breadcrumbs: breadcrumbs,
+                            managers: result
+                        });
+                    }
+                    else{
+                        res.status(404).render('error404');
+                    }
+                });
+            }
+            else{
+                res.status(404).render('error404');
+            }
+        });
+        conn.release();
+    });
+
+});
+
+managers.post('/manager-id&:test_id', function(req, res){
+
+    var my_id = settings.get_manager_id();
+    var your_id = req.params.test_id.split('=')[1];
+    if(my_id == your_id) res.send(true);
     else res.send(false);
 
 });
