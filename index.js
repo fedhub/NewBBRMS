060fd4f7fd66c04f54e4019cbb2bd365d77d60d9
shimonsw <shimi0007@hotmail.com>
2015-05-14 01:35:41 +0300

    WIP - MS Managers

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ app.use(express.static(path.join(__dirname, 'includes')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // define routes
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 
 app.use(require('./mysql'));
 app.use(require('./mobile_authentication_routers'));
@@ -36,14 +42,6 @@ app.use(require('./authentication'));
 app.use(require('./routers'));
 app.use(require('./functions'));
 app.use(require('./managers'));
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
 
 //app.use(require('./authentication'));
 //app.use(require('./routers'));
