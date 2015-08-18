52b9564f5d539470db0b5cea7ae00653046b5f7d
shimonsw <shimi0007@hotmail.com>
2015-08-18 18:45:01 +0300

    WIP - https

--- includes/js/credit-payment.js
+++ includes/js/credit-payment.js
@@ -4,7 +4,17 @@ $(document).ready(function() {
     var width = $(document).width();
     $('body').css('height', height);
     $('body').css('width', width);
-    $('iframe').css('height', '100%');
-    $('iframe').css('width', '100%');
+    $('iframe').css({'height': '100%',
+    'width': '100%',
+    '-moz-transform': 'scale(0.25, 0.25)',
+    '-webkit-transform': 'scale(0.25, 0.25)',
+    '-o-transform': 'scale(0.25, 0.25)',
+    '-ms-transform': 'scale(0.25, 0.25)',
+    'transform': 'scale(0.25, 0.25)',
+    '-moz-transform-origin': 'top left',
+    '-webkit-transform-origin': 'top left',
+    '-o-transform-origin': 'top left',
+    '-ms-transform-origin': 'top left',
+    'transform-origin': 'top left'});
 
 });
